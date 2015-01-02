0b7c0180f82d7395d8d7f3c286975c547d8d16ff
sherwinp <sherwinprice@hotmail.com>
2015-01-02 00:30:52 -0500

    add further workflow

---
+++ src/test/java/TestAuth.java
@@ -0,0 +1,18 @@
+import java.util.logging.Logger;
+
+import org.jboss.security.auth.login.*;
+
+import org.jboss.security.util.*;
+import org.jboss.security.auth.spi.Util;
+
+public class TestAuth{
+	private static final Logger log = Logger.getLogger(TestAuth.class.getName());
+public static void main(String [ ] args)
+{
+
+
+	
+	String hashedPassword = org.jboss.security.auth.spi.Util.createPasswordHash("MD5", "BASE64", null, null, "userbob");
+	log.info(String.format("hashed password: %s", hashedPassword));
+}
+}
