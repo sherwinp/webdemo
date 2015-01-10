e34753f56497dc3c4cd8029b06cd83ac03542fb4
sherwinp <sherwinprice@hotmail.com>
2015-01-10 12:49:49 -0500

    added module loader.

--- src/main/webapp/resources/scripts/login.js
+++ src/main/webapp/resources/scripts/login.js
@@ -1,17 +1,23 @@
-function loginwatch(){
-	$.ajax({
-		   url: 'authenticated',
-		   data: {
-		      format: 'json'
-		   },
-		   error: function() {
-			   console.log( "error ajax: loginwatch. " );
-		   },
-		   dataType: 'json',
-		   success: function(data) {
-			   console.log( "success login -- try redirect." );
-			   window.location.href = "index.html";
-		   },
-		   type: 'GET'
-		});
-}
+define(['jquery'], function (){
+	var loginwatch=function(){
+		$.ajax({
+			   url: 'authorized',
+			   data: {
+			      format: 'json'
+			   },
+			   error: function() {
+				   console.log( "error ajax: loginwatch. " );
+			   },
+			   dataType: 'json',
+			   success: function(data) {
+				   console.log( "success login -- try redirect." );
+				   window.location.href = "index.html";
+			   },
+			   type: 'GET'
+			});
+	};
+	
+	return { 
+		loginwatch:loginwatch 
+	};
+});
--- src/main/webapp/resources/scripts/main.js
+++ src/main/webapp/resources/scripts/main.js
@@ -1,14 +1,6 @@
 require.config({
-paths: {
-jquery: 'jquery'
-}
-});
-
-require([
-//Load our app module and pass it to our definition function
-'app',
-], function(App){
-//The "app" dependency is passed in as "App"
-//Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
-App.initialize();
+paths: { 
+		jquery: 'jquery',
+		handlebars: 'handlebars' 
+		}
 });
